e1b28d1196586419f1179176fee5462cf8af5584
Emilly <emillyquessada26@gmail.com>
2025-10-20 10:04:49 -0300

    Adicionando fução de listar produtos ✔

--- Back-End/funcao.py
+++ Back-End/funcao.py
@@ -36,3 +36,19 @@ def inserir_produtos(nome, categoria, preco, quantidade):
             cursor.close()
             conexao.close()
 
+def listar_produtos():
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "SELECT * FROM produtos ORDER BY id"
+            )
+            return cursor.fetchall()
+        except Exception as erro:
+            print(f"Erro ao tentar listar produtos: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+
+

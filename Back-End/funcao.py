b69054f2e5636aeccf7aaf5caceef1a5f9be24c1
Emilly <emillyquessada26@gmail.com>
2025-10-20 09:54:12 -0300

    Adicionando função inserir produtos ✔

--- Back-End/funcao.py
+++ Back-End/funcao.py
@@ -5,8 +5,8 @@ def criar_tabela():
     if conexao: 
         try:
             cursor.execute("""
-                CREATE TABLE produtos (
-                id INT PRIMARY KEY,
+                CREATE TABLE IF NOT EXISTS produtos (
+                id SERIAL PRIMARY KEY,
                 nome VARCHAR(100) NOT NULL,
                 categoria VARCHAR(50),
                 preco DECIMAL(10,2),
@@ -19,4 +19,20 @@ def criar_tabela():
         finally:
             cursor.close()
             conexao.close()
-criar_tabela()
+
+
+def inserir_produtos(nome, categoria, preco, quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "INSERT INTO produtos (nome, categoria, preco, quantidade) VALUES (%s, %s, %s, %s)",
+                (nome, categoria, preco, quantidade)
+                )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao inserir produto: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+

875d64768a71083f3e0f9d008ee2276cb0f8c1d4
Emilly <emillyquessada26@gmail.com>
2025-10-20 10:40:18 -0300

    Adicionando função de deletar ✔

--- Back-End/funcao.py
+++ Back-End/funcao.py
@@ -66,3 +66,18 @@ def atualizar_produtos(id_produto, novo_preco, nova_quantidade):
         finally:
             cursor.close()
             conexao.close()
+
+def deletar_produtos(id_produto):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(" DELETE FROM produtos WHERE id = %s",
+            (id_produto,)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao deletar produto: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
